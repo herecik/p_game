f732443dc392d224e24aa2f44c4d7b36376065bd
Jan Dejmal <dejmal@logio.cz>
2023-07-27 12:19:13 +0200

    Fixed classed

--- p_game.py
+++ p_game.py
@@ -1,13 +1,9 @@
 import constant as c
-#class hero
-class Hero:
-    type = c.PLAYER
-    attack = 1
-    health = 10
-    
-    def __init__(self, name, difficulty):
+class Character:
+    def __init__(self, name, health, attack):
         self.name = name
-        self.difficulty = difficulty
+        self.health = health
+        self.attack = attack
     
     def buff_atk(self, buff):
         self.attack = self.attack + buff
@@ -17,27 +13,24 @@ class Hero:
     
     def show_stats(self):
         print("attack>" + str(self.attack) + " HP>" + str(self.health))
+    
+#class Hero inherits Character 
+class Hero(Character):
+    type = c.PLAYER
+    
+    
 
-#class monster 
-class Monster:
+#class Monster inherits Monster 
+class Monster(Character):
     type = c.MONSTER      
-    def __init__(self, name, health, attack):
-        self.name = name
-        self.health = health
-        self.attack = attack
-        #self.scream()
         
     def scream(self):
         print("WAAAAAA")  
-    
-    #will be fixed player and monster will inherit the same class    
-    def show_stats(self):
-        print("attack>" + str(self.attack) + " HP>" + str(self.health))
 
 class Coms:
     def start_game(self):
         self.i_name = input("Zvol si své jméno: ")
-        self.i_difficulty = input("Vyber obtížnost 1-5: ")
+        #self.i_difficulty = input("Vyber obtížnost 1-5: ")
         
     def game_over(self):
         print("Dostali tě, zkus to znovu")
@@ -45,8 +38,13 @@ class Coms:
     def monster_dead(self, monster_name):
         print(monster_name + " byl poražen, pokračuješ dál")
         
-def check_if_dead(character):
+    def round(self, num):
+        print("Kolo číslo: " + str(num))
+        
+        
+def check_if_dead(character ):
     notification = Coms()
+    
     if character.health == 0:
         if character.type == c.MONSTER:
             notification.monster_dead(character.name)
@@ -60,31 +58,23 @@ def check_if_dead(character):
 
 
 
-def encounter(monster, player, i = 0):
-
-    #player atttacks a monster
-    print(i)
-    
+def encounter(monster, player, i):
+    notification = Coms()
+    notification.round(i)
     monster.health = monster.health - player.attack
     player.health = player.health - monster.attack
-    i += 1
     
     if not check_if_dead(monster):
-        print("monster live")
+        #print("monster live")
         if not check_if_dead(player):
-            print("player live")
-            encounter(monster, player)
-    
-    
-    
-    
-    
+            #print("player live")
+            encounter(monster, player, i + 1)
 
 chat = Coms()
 chat.start_game()
-player1 = Hero(chat.i_name, chat.i_difficulty)
-monster1 = Monster("Vlk", 4, 5)
-encounter(monster1, player1)
+player1 = Hero(chat.i_name, 5, 1)
+monster1 = Monster("Vlk", 6, 1)
+encounter(monster1, player1, 1)
 
 
 
