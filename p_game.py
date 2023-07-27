0c40ce6e57d1940fe6bcb80041be1489bec4fb27
Jan Dejmal <dejmal@logio.cz>
2023-07-27 14:23:43 +0200

    Constant encounter generation

--- p_game.py
+++ p_game.py
@@ -1,4 +1,6 @@
 import constant as c
+from random import randrange
+
 class Character:
     def __init__(self, name, health, attack):
         self.name = name
@@ -6,14 +8,14 @@ class Character:
         self.attack = attack
     
     def buff_atk(self, buff):
-        self.attack = self.attack + buff
+        self.attack += buff
     
     def buff_hp(self, buff):
-        self.health = self.health + buff
+        self.health += buff
     
     def show_stats(self, arg):
         if arg == 1:
-            return 1#"Útok: " + str(self.attack) + " HP: " + str(self.health))
+            return 1 #"Útok: " + str(self.attack) + " HP: " + str(self.health))
         elif arg == 2:
             return self.health
         elif arg == 3:
@@ -21,17 +23,20 @@ class Character:
         else:
             print("Invalid argument in show_stats()")
     
-#class Hero inherits Character 
+#class Hero dědí Character 
 class Hero(Character):
     type = c.PLAYER
+    alive = 1
     
-#class Monster inherits Monster 
+#class Monster dědí Character 
 class Monster(Character):
     type = c.MONSTER      
-        
+
+#zatím nepoužívám ale může řvát     
     def scream(self):
         print("WAAAAAA")  
 
+#tuto třídu používáme pro message do chatu
 class Coms:
     def start_game(self):
         self.i_name = input("Zvol si své jméno: ")
@@ -39,21 +44,25 @@ class Coms:
         
     def game_over(self):
         print("Dostali tě, zkus to znovu")
+    
+    def next_prompt(self):
+        input("Zmáčkni Enter pro pokračování...")
         
     def monster_dead(self, monster_name):
         print(monster_name + " byl poražen, pokračuješ dál")
+        self.next_prompt()
         
     def round(self, num):
         print("Kolo číslo: " + str(num))
-        
-        
-def check_if_dead(character ):
+    
+def check_if_dead(character):
     notification = Coms()
     
     if character.health <= 0:
         if character.type == c.MONSTER:
             notification.monster_dead(character.name)
         elif character.type == c.PLAYER:
+            character.alive = 0
             notification.game_over()
         else:
             print("Bad value")
@@ -61,33 +70,42 @@ def check_if_dead(character ):
         return 1
     return 0
 
-
-
 def fight(monster, player, i):
     notification = Coms()
     notification.round(i)
     
-    print(monster.name + " má " + str(monster.show_stats(2)) + " HP")
-    print(player.name + " má " + str(player.show_stats(2)) + " HP")
-    
     monster.health = monster.health - player.attack
     player.health = player.health - monster.attack
     
+    print(monster.name + " má " + str(monster.show_stats(2)) + " HP")
+    print(player.name + " má " + str(player.show_stats(2)) + " HP")
+      
     if not check_if_dead(monster):
         #print("monster live")
         if not check_if_dead(player):
             #print("player live")
+            notification.next_prompt()
             fight(monster, player, i + 1)
             
 def encounter(monster, player):
+    chat = Coms()
     print("Narazil jsi na " + monster.name + "! Útok: " + str(monster.attack) + " HP: " + str(monster.health))
+    chat.next_prompt()
     fight(monster, player, 1)
-
+    
+def generate_encounter(player):
+    encounter(Monster("placeholder", randrange(1, 6), randrange(1, 2)), player)
+    
 chat = Coms()
 chat.start_game()
-player1 = Hero(chat.i_name, 5, 1)
-monster1 = Monster("Vlk", 6, 1)
-encounter(monster1, player1)
+player1 = Hero(chat.i_name, 10, 2)
+
+#TODO udělat rozdělení např na dungeony
+
+#encounter generation dokud hráč žije
+while player1.alive:
+    generate_encounter(player1)
+
 
 
 
